2f7ad232de37d198779fcfd7d9b09c6f74e20d89
Joshua Grant <joshua.m.grant@gmail.com>
2016-08-23 16:06:51 -0400

    tests for mixed_metaphors.misc

--- tests/test_mixed_metaphors.py
+++ tests/test_mixed_metaphors.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for mixed_metaphors.misc check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.mixed_metaphors import misc as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for mixed_metaphors.misc."""
 
     __test__ = True
 
@@ -16,7 +16,17 @@ class TestCheck(Check):
         """Bolierplate."""
         return chk
 
-    def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
-        assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+    def test_smoke_bottleneck(self):
+        """Basic smoke test for check_bottleneck."""
+        assert chk.check_bottleneck(
+            """Smoke phrase with nothing flagged.""") == []
+        assert chk.check_bottleneck(
+            """The project produced a huge bottleneck.""") != []
+
+    def test_smoke_misc(self):
+        """Basic smoke test for check_misc."""
+        assert chk.check_misc(
+            """Smoke phrase with nothing flagged.""") == []
+        assert chk.check_misc(
+            """Writing tests is not rocket surgery.""") != []
+
