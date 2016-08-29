7ddfda3779afd4d3ed2540fba8745a2f9807b989
Josh Grant <joshua.grant@caseware.com>
2016-08-29 16:56:51 -0400

    tests for redundancy.misc

--- tests/test_redundancy_misc.py
+++ tests/test_redundancy_misc.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for redundancy.misc check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.redundancy import misc as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for redundancy.misc."""
 
     __test__ = True
 
@@ -16,7 +16,28 @@ class TestCheck(Check):
         """Bolierplate."""
         return chk
 
-    def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
+    def test_smoke_check(self):
+        """Basic smoke test for redundancy.misc.check."""
         assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+        assert not self.passes("""The table was rectangular in shape.""")
+
+    def test_smoke_garner(self):
+        """Basic smoke test for redundancy.misc.check_garner."""
+        assert chk.check_garner(
+            """Smoke phrase with nothing flagged.""") == []
+        assert chk.check_garner(
+            """It was blatantly obvious what to do next.""") != []
+
+    def test_smoke_nordquist(self):
+        """Basic smoke test for redundancy.misc.check_norquist."""
+        assert chk.check_nordquist(
+            """Smoke phrase with nothing flagged.""") == []
+        assert chk.check_nordquist(
+            """Taking the package was absolutely essential.""") != []
+
+    def test_smoke_atd(self):
+        """Basic smoke test for redundancy.misc.check_norquist."""
+        assert chk.check_atd(
+            """Smoke phrase with nothing flagged.""") == []
+        assert chk.check_atd(
+            """He often repeated the old adage.""") != []
