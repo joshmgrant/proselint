39a8826e1b8fff8d608abafc9113f4aff18c134b
Josh Grant <joshua.grant@caseware.com>
2016-08-30 16:00:26 -0400

    tests for redundancy.ras_syndrome

--- tests/test_redundancy_ras_syndrome.py
+++ tests/test_redundancy_ras_syndrome.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for redundancy.ras_syndrome check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.redundancy import ras_syndrome as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for redundancy.ras_syndrome."""
 
     __test__ = True
 
@@ -17,6 +17,6 @@ class TestCheck(Check):
         return chk
 
     def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
+        """Basic smoke test for redundancy.ras_syndrome."""
         assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+        assert not self.passes("""Please enter your PIN number.""")
