befebfa153fc4ec859c022b99eacd1ad5b4a6170
Josh Grant <joshua.grant@caseware.com>
2016-08-31 16:20:03 -0400

    test for terms.animal_adjectives

--- tests/test_terms_animal_adjectives.py
+++ tests/test_terms_animal_adjectives.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for terms.animal_adjectives check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.terms import animal_adjectives as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for terms.animal_adjectives."""
 
     __test__ = True
 
@@ -17,6 +17,6 @@ class TestCheck(Check):
         return chk
 
     def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
+        """Basic smoke test for terms.animal_adjectives."""
         assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+        assert not self.passes("""It was some bird-like creature.""")
