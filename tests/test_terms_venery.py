5e23e0604fda7fe4ce49495901c1557d64ca4e2b
Josh Grant <joshua.grant@caseware.com>
2016-08-31 16:33:19 -0400

    tests for terms.venery

--- tests/test_terms_venery.py
+++ tests/test_terms_venery.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for terms.venery check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.terms import venery as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for terms.venery."""
 
     __test__ = True
 
@@ -17,6 +17,6 @@ class TestCheck(Check):
         return chk
 
     def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
+        """Basic smoke test for terms.venery."""
         assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+        assert not self.passes("""There was a group of alligators.""")
