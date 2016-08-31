7cbdf23ec594127ac1219c201c26f09aff85a12c
Josh Grant <joshua.grant@caseware.com>
2016-08-31 16:29:50 -0400

    tests for terms.eponymous_adjectives

--- tests/test_terms_eponymous_adjectives.py
+++ tests/test_terms_eponymous_adjectives.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for terms.eponymous_adjectives check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.terms import eponymous_adjectives as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for terms.eponymous_adjectives."""
 
     __test__ = True
 
@@ -17,6 +17,6 @@ class TestCheck(Check):
         return chk
 
     def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
+        """Basic smoke test for terms.eponymous_adjectives."""
         assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+        assert not self.passes("""The writing wasn't Shakespearian.""")
