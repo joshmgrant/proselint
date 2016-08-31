834d596f8f6f42d07810729e13d3bb2836595260
Josh Grant <joshua.grant@caseware.com>
2016-08-31 16:27:39 -0400

    tests for terms.denizen_labels

--- tests/test_terms_denizen_labels.py
+++ tests/test_terms_denizen_labels.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for terms.denizen_labels check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.terms import denizen_labels as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for terms.denizen_labels."""
 
     __test__ = True
 
@@ -17,6 +17,6 @@ class TestCheck(Check):
         return chk
 
     def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
+        """Basic smoke test for terms.denizen_labels."""
         assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+        assert not self.passes("""He was definitely a Hong Kongite.""")
