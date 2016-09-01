946edd68fc91361568600932f815f65455d1a2c5
Josh Grant <joshua.grant@caseware.com>
2016-09-01 16:04:31 -0400

    test for typography.exclamation

--- tests/test_typography_exclamation.py
+++ tests/test_typography_exclamation.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Tests for typography.exclamation check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.typography import exclamation as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for typography.exclamation."""
 
     __test__ = True
 
@@ -16,7 +16,20 @@ class TestCheck(Check):
         """Bolierplate."""
         return chk
 
-    def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
-        assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+    def test_smoke_repeated_exclamations(self):
+        """Basic smoke test for 
+        typography.exclamation.check_repeated_exclamations.
+        """
+        assert chk.check_repeated_exclamations(
+            """Smoke phrase with nothing flagged.""") == []
+        assert chk.check_repeated_exclamations(
+            """I'm really excited!!""") != []
+
+    def test_smoke_exclamations_ppm(self):
+        """Basic smoke test for 
+        typography.exclamation.check_exclamations_ppm.
+        """
+        assert chk.check_exclamations_ppm(
+            """Smoke phrase with nothing flagged.""") == []
+        assert chk.check_exclamations_ppm(
+            """I'm really excited! Really!""") != []
