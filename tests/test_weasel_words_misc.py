abd757fda6693b5ebfea1f49d26640140860245a
Josh Grant <joshua.grant@caseware.com>
2016-09-01 16:20:31 -0400

    tests for weasel_words checks

--- tests/test_weasel_words_misc.py
+++ tests/test_weasel_words_misc.py
@@ -1,13 +1,14 @@
-"""Tests for cursing.nfl check."""
+"""Tests for weasel_words.misc check."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
-
+from proselint.checks.weasel_words import misc as chk
+from nose import SkipTest
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for weasel_words.misc."""
+    raise SkipTest
 
     __test__ = True
 
@@ -17,6 +18,6 @@ class TestCheck(Check):
         return chk
 
     def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
+        """Basic smoke test for weasel_words.misc."""
         assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+        # FIXME add test when check is implemented
