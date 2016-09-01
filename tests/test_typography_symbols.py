ff46b9a7b05f2ab919aaad2eabcc10c117c2415d
Josh Grant <joshua.grant@caseware.com>
2016-09-01 16:07:11 -0400

    good work, me

--- tests/test_typography_symbols.py
+++ tests/test_typography_symbols.py
@@ -1,13 +1,13 @@
-"""Tests for cursing.nfl check."""
+"""Test Butterick's symbols."""
 from __future__ import absolute_import
 
 from .check import Check
 
-from proselint.checks.cursing import nfl as chk
+from proselint.checks.typography import symbols as chk
 
 
 class TestCheck(Check):
-    """The test class for cursing.nfl."""
+    """The test class for typography.symbols."""
 
     __test__ = True
 
@@ -16,7 +16,35 @@ class TestCheck(Check):
         """Bolierplate."""
         return chk
 
-    def test_smoke(self):
-        """Basic smoke test for cursing.nfl."""
-        assert self.passes("""Smoke phrase with nothing flagged.""")
-        assert not self.passes("""The QB is named ball licker.""")
+    def test_ellipsis(self):
+        """Find ... in a string."""
+        assert chk.check_ellipsis("""The long and winding road...""")
+
+    def test_copyright(self):
+        """Find a (c) or (C) in a string."""
+        assert chk.check_copyright_symbol("""Show me the money! (C)""")
+        assert chk.check_copyright_symbol("""Show me the money! (c)""")
+
+    def test_trademark(self):
+        """Find a (TM) or (tm) in a string."""
+        assert chk.check_trademark_symbol("""The Fresh Maker (TM)""")
+        assert chk.check_trademark_symbol("""The Fresh Maker (tm)""")
+
+    def test_registered_trademark(self):
+        """Find a (r) or (R) in a string."""
+        assert chk.check_registered_trademark_symbol("""Just Do It (R)""")
+        assert chk.check_registered_trademark_symbol("""Just Do It (r)""")
+
+    def test_sentence_spacing(self):
+        """Find a sentence followed by three or more spaces."""
+        assert chk.check_sentence_spacing(
+            """This is a sentence.   This is another.""")
+
+    def test_multiplication(self):
+        """Find an x between two digits."""
+        assert chk.check_multiplication_symbol(
+            """It is obvious that 2 x 2 = 4.""")
+
+    def test_curly_quotes(self):  # FIXME
+        """Find "" quotes in a string."""
+        pass
